26e4faf8ae1e34263ca2d68dd075f33d42e8eaa5
ogike <77450926+ogike@users.noreply.github.com>
2021-09-23 16:17:52 +0200

    update

---
+++ Webprog/gyak2/script.js
@@ -0,0 +1,46 @@
+const header = ['Cegnev', 'Adoszam', 'Ipar']
+
+const cegek = [
+    {
+        Cegnev: 'HZ Kft',
+        Adoszam: '111111',
+        Ipar: 'IT'
+    }, 
+    {
+        Cegnev: 'Deak és Társai Zrt',
+        Adoszam: '666666',
+        Ipar: 'Fashion'
+    },
+    {
+        Cegnev: 'ELTE IK',
+        Adoszam: '000000',
+        Ipar: 'IT'
+    }
+]
+
+let tablazat = document.createElement('table')
+
+let headerSor = document.createElement('tr')
+
+header.forEach(element => {
+    let sor = document.createElement('th')
+    sor.innerHTML = element
+    headerSor.appendChild(sor)
+})
+
+tablazat.appendChild(headerSor)
+
+cegek.forEach(element => {
+    let ujsor = document.createElement('tr')
+    /*header.forEach(elem => {
+        let ujelem = document.createElement('td')
+        ujelem.innerText = element[elem]
+        ujsor.appendChild(ujelem)
+    })*/
+    //ujsor.innerHTML = `<td>${element.Cegnev}</td><td>${element.Adoszam}</td><td>${element.Ipar}</td>`
+    ujsor.innerHTML = header.reduce((a, b) =>{ return a + `<td>${element[b]}</td>`}, '')
+    tablazat.appendChild(ujsor)
+})
+
+
+document.body.appendChild(tablazat)
---
+++ Webprog/gyak2/script2.js
@@ -0,0 +1,10 @@
+let szoveg = 'A hibrid oktatás nagyon jó :)'
+
+for(let i = 10; i < 100; i+=10){
+    let bekezdes = document.createElement('p')
+    bekezdes.innerHTML = szoveg
+    bekezdes.style.fontSize = `${i}px`
+    bekezdes.style.color = `#FF${i}FF`
+    
+    document.body.appendChild(bekezdes)
+}
