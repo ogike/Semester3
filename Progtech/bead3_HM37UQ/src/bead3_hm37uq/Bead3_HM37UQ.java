405fde9a4502cee7c8574ab185d7d00ed38bea3f
ogike <77450926+ogike@users.noreply.github.com>
2021-11-30 23:35:34 +0100

    progtech bead3 start

---
+++ Progtech/bead3_HM37UQ/src/bead3_hm37uq/Bead3_HM37UQ.java
@@ -0,0 +1,19 @@
+/*
+
+ */
+package bead3_hm37uq;
+
+/**
+ *
+ * @author ogike
+ */
+public class Bead3_HM37UQ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
